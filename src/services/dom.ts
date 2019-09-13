73b7f24e3c541dc3cefddf073f5680110186f1c1
DavidJones <qowera@qq.com>
2019-09-14 02:07:08 +0800

    删除 不必要文件

--- src/services/dom.ts
+++
@@ -1,4 +0,0 @@
-export const create = (tag: string, classes: string | string[], container: HTMLElement) => {
-  const element = document.createElement(tag)
-  element.className = Array.isArray(classes) ? classes.join(' ') : classes
-}
